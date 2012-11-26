c5efb6f266a44e2bfbf9c03cce9666744992edc0
Cordobo <cordobo@web.de>
2012-11-26 10:01:37 +0100

    Breadcrumbs

--- single.php
+++ single.php
@@ -1,77 +1,107 @@
 <?php get_header(); ?>
 
-	<div id="container">
-		<div id="content">
+<div id="container">
+<div id="content">
 
-	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-
-		<div <?php post_class() ?> id="post-<?php the_ID(); ?>">
-			<h1><?php the_title(); ?></h1>
-			<small class="meta">
-  	<span class="alignleft">
-            <?php the_time(__('F jS, Y', 'default')) ?> <?php _e('by', 'default'); ?> <?php the_author() ?>
-            <?php edit_post_link(__( 'Edit this entry', 'default' ), ' | ', ''); ?> <?php if(function_exists('wp_print')) { print_link(); } ?>
+    <div id="breadcrumb" itemprop="breadcrumb">
+        You are here: 
+        <span itemscope itemtype="http://data-vocabulary.org/Breadcrumb">
+            <a href="<?php echo esc_url( home_url( '/' ) ); ?>" itemprop="url">
+                <span itemprop="title">Home</span>
+            </a>
+            &rsaquo;
         </span>
-        
-        <?php if ('open' == $post->comment_status) : ?>
-        <span class="alignright">
-            <a href="#comments" class="button-style" rel="nofollow"><?php _e('Leave a reply', 'default'); ?> &raquo;</a>
+        <span itemscope itemtype="http://data-vocabulary.org/Breadcrumb">
+            <?php 
+            $category = get_the_category(); 
+            if($category[0]){
+                echo '<a href="'.get_category_link($category[0]->term_id ).'" itemprop="url"><span itemprop="title">'.$category[0]->cat_name.'</span></a>';
+            }
+            ?>          
+            &rsaquo;
         </span>
-        <?php endif; ?>
-      </small>
+        <span itemscope itemtype="http://data-vocabulary.org/Breadcrumb">
+            <a href="<?php the_permalink() ?>" itemprop="url">
+                <span itemprop="title"><?php the_title_attribute(); ?></span>
+            </a>
+        </span>
+    </div>
+    
+   
+    
 
-			<div class="entry">
-				<?php the_content(); ?>
-				<?php wp_link_pages(array('before' => '<div class="page-link clearfix"><strong>Pages:</strong>', 'after' => '</div>', 'next_or_number' => 'number', 'pagelink' => '<span>%</span>')); ?>
+  
+    
+<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-      	<ul class="previousnext clearfix">
-      		<?php previous_post_link('<li class="previous_post">%link</li>', '<span>' . (__('Previous Entry', 'default')) . ':</span> %title'); ?>
-      		<?php next_post_link('<li class="next_post">%link</li>', '<span>' . (__('Next Entry', 'default')) . ':</span> %title'); ?>
-      	</ul>
-			</div>
-			
-			<div class="postmetadata">
-			  <p class="categories">
-			    <?php _e('Posted in ', 'default' ); the_category(', '); ?>
-        </p>
-  			<?php the_tags('<p class="tags">Tags: ', ' ', '</p>'); ?>
-        <p class="infos">
-						<?php _e('You can follow any responses to this entry through the', 'default'); ?> <a href="<?php echo get_post_comments_feed_link() ?>" rel="nofollow"><?php _e('RSS 2.0 Feed', 'default'); ?></a>. 
+        <div <?php post_class() ?> id="post-<?php the_ID(); ?>">
+                <h1><?php the_title(); ?></h1>
+                <small class="meta">
+<span class="alignleft">
+    <?php the_time(__('F jS, Y', 'default')) ?> <?php _e('by', 'default'); ?> <?php the_author() ?>
+    <?php edit_post_link(__( 'Edit this entry', 'default' ), ' | ', ''); ?> <?php if(function_exists('wp_print')) { print_link(); } ?>
+</span>
 
-						<?php if (('open' == $post-> comment_status) && ('open' == $post->ping_status)) {
-							// Both Comments and Pings are open ?>
-							<?php _e('You can', 'default'); ?> <a href="#respond"><?php _e('leave a response', 'default'); ?></a> <?php _e(', or', 'default');?> <a href="<?php trackback_url(); ?>" rel="trackback nofollow"><?php _e('trackback', 'default'); ?></a> <?php _e('from your own site', 'default'); ?>.
+<?php if ('open' == $post->comment_status) : ?>
+<span class="alignright">
+    <a href="#comments" class="button-style" rel="nofollow"><?php _e('Leave a reply', 'default'); ?> &raquo;</a>
+</span>
+<?php endif; ?>
+</small>
 
-						<?php } elseif (!('open' == $post-> comment_status) && ('open' == $post->ping_status)) {
-							// Only Pings are Open ?>
-							<?php _e('Responses are currently closed, but you can', 'default'); ?> <a href="<?php trackback_url(); ?> " rel="trackback nofollow"><?php _e('trackback', 'default'); ?></a> <?php _e('from your own site', 'default'); ?>.
+                <div class="entry">
+                        <?php the_content(); ?>
+                        <?php wp_link_pages(array('before' => '<div class="page-link clearfix"><strong>Pages:</strong>', 'after' => '</div>', 'next_or_number' => 'number', 'pagelink' => '<span>%</span>')); ?>
 
-						<?php } elseif (('open' == $post-> comment_status) && !('open' == $post->ping_status)) {
-							// Comments are open, Pings are not ?>
-							<?php _e('You can skip to the end and leave a response. Pinging is currently not allowed.', 'default'); ?>
+<ul class="previousnext clearfix">
+        <?php previous_post_link('<li class="previous_post">%link</li>', '<span>' . (__('Previous Entry', 'default')) . ':</span> %title'); ?>
+        <?php next_post_link('<li class="next_post">%link</li>', '<span>' . (__('Next Entry', 'default')) . ':</span> %title'); ?>
+</ul>
+                </div>
 
-						<?php } elseif (!('open' == $post-> comment_status) && !('open' == $post->ping_status)) {
-							// Neither Comments, nor Pings are open ?>
-							<?php _e('Both comments and pings are currently closed.', 'default'); ?>
+                <div class="postmetadata">
+                  <p class="categories">
+                    <?php _e('Posted in ', 'default' ); the_category(', '); ?>
+</p>
+                <?php the_tags('<p class="tags">Tags: ', ' ', '</p>'); ?>
+<p class="infos">
+                                        <?php _e('You can follow any responses to this entry through the', 'default'); ?> <a href="<?php echo get_post_comments_feed_link() ?>" rel="nofollow"><?php _e('RSS 2.0 Feed', 'default'); ?></a>. 
 
-						<?php } edit_post_link(__( 'Edit this entry', 'default' ),'','.'); ?>
-					</p>
-				</div>
+                                        <?php if (('open' == $post-> comment_status) && ('open' == $post->ping_status)) {
+                                                // Both Comments and Pings are open ?>
+                                                <?php _e('You can', 'default'); ?> <a href="#respond"><?php _e('leave a response', 'default'); ?></a> <?php _e(', or', 'default');?> <a href="<?php trackback_url(); ?>" rel="trackback nofollow"><?php _e('trackback', 'default'); ?></a> <?php _e('from your own site', 'default'); ?>.
 
-      <?php get_template_part( 'ads', 'middle' ); ?>
+                                        <?php } elseif (!('open' == $post-> comment_status) && ('open' == $post->ping_status)) {
+                                                // Only Pings are Open ?>
+                                                <?php _e('Responses are currently closed, but you can', 'default'); ?> <a href="<?php trackback_url(); ?> " rel="trackback nofollow"><?php _e('trackback', 'default'); ?></a> <?php _e('from your own site', 'default'); ?>.
 
-		</div>
+                                        <?php } elseif (('open' == $post-> comment_status) && !('open' == $post->ping_status)) {
+                                                // Comments are open, Pings are not ?>
+                                                <?php _e('You can skip to the end and leave a response. Pinging is currently not allowed.', 'default'); ?>
 
-  	<?php comments_template('', true); ?> 	 
+                                        <?php } elseif (!('open' == $post-> comment_status) && !('open' == $post->ping_status)) {
+                                                // Neither Comments, nor Pings are open ?>
+                                                <?php _e('Both comments and pings are currently closed.', 'default'); ?>
 
-	<?php endwhile; else: ?>
+                                        <?php } edit_post_link(__( 'Edit this entry', 'default' ),'','.'); ?>
+                                </p>
+                        </div>
 
-    <?php get_template_part( 'content', 'missing' ); ?>
+<?php get_template_part( 'ads', 'middle' ); ?>
+
+        </div>
+
+<?php comments_template('', true); ?> 	 
+
+<?php endwhile; else: ?>
+
+<?php get_template_part( 'content', 'missing' ); ?>
 
 <?php endif; ?>
 
-		</div><!-- #content -->
-	</div><!-- #container -->
+
+</div><!-- #content -->
+</div><!-- #container -->
 
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
