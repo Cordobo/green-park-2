38db51a176ef28d307222c57aa04e34bf9012fbd
Cordobo <cordobo@web.de>
2012-12-13 22:16:59 +0100

    wp_enqueue_style() - stylesheet referenced via get_stylesheet_uri() - fixes #16

--- functions.php
+++ functions.php
@@ -2,23 +2,18 @@
 if ( ! isset( $content_width ) )
 	$content_width = 607;
 
-/**
- * Sets up theme defaults and registers the various WordPress features that
- * Twenty Twelve supports.
+/*
+ * Sets up theme defaults and registers the various WordPress features
  *
  * @uses load_theme_textdomain() For translation/localization support.
  * @uses add_editor_style() To add a Visual Editor stylesheet.
  * @uses add_theme_support() To add support for post thumbnails, automatic feed links,
  * 	custom background, and post formats.
- * @uses register_nav_menu() To add support for navigation menus.
  * @uses set_post_thumbnail_size() To set a custom post thumbnail size.
- *
- * @since Twenty Twelve 1.0
+ * @uses register_nav_menu() To add support for navigation menus.
  */
 function greenpark_setup() {
-	/*
-	 * Translations (located at /languages/
-	 */
+	// Translations located at /languages/
 	load_theme_textdomain( 'greenpark', get_template_directory() . '/languages' );
 
 	// This theme styles the visual editor with editor-style.css to match the theme style.
@@ -29,15 +24,7 @@ function greenpark_setup() {
 
 	// This theme supports a variety of post formats.
 	add_theme_support( 'post-formats', array( 'aside', 'image', 'link', 'quote', 'status' ) );
-
-        // This theme uses wp_nav_menu() in four locations.
-        register_nav_menus( array(
-            'primary' => __( 'Primary Navigation', 'greenpark' ),
-            'accessibility_menu' => __( 'Accessibility Menu', 'greenpark' ),
-            'sidebar_menu' => __( 'Sidebar Menu', 'greenpark' ),
-            'footer_menu' => __( 'Footer Menu', 'greenpark' )
-        ) );
-
+        
 	/*
 	 * This theme supports custom background color and image, and here
 	 * we also set up the default background color.
@@ -49,10 +36,50 @@ function greenpark_setup() {
 	// This theme uses a custom image size for featured images, displayed on "standard" posts.
 	add_theme_support( 'post-thumbnails' );
 	set_post_thumbnail_size( 624, 9999 ); // Unlimited height, soft crop
+
+        // This theme uses wp_nav_menu() in four locations.
+        register_nav_menus( array(
+            'primary' => __( 'Primary Navigation', 'greenpark' ),
+            'accessibility_menu' => __( 'Accessibility Menu', 'greenpark' ),
+            'sidebar_menu' => __( 'Sidebar Menu', 'greenpark' ),
+            'footer_menu' => __( 'Footer Menu', 'greenpark' )
+        ) );
+
 }
 add_action( 'after_setup_theme', 'greenpark_setup' );
 
-/////////// OLD
+
+
+// Adds support for a custom header image.
+// require( get_template_directory() . '/inc/custom-header.php' );
+
+// Enqueues scripts and styles for front-end.
+function greenpark_scripts_styles() {
+	global $wp_styles;
+
+	/*
+	 * Adds JavaScript to pages with the comment form to support
+	 * sites with threaded comments (when in use).
+	 */
+	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) )
+		wp_enqueue_script( 'comment-reply' );
+
+
+	// Adds JavaScript for handling the navigation menu hide-and-show behavior.
+
+	// Fonts
+
+	// Loads our main stylesheet.
+	wp_enqueue_style( 'greenpark-style', get_stylesheet_uri() );
+
+	// Loads the Internet Explorer specific stylesheet.
+
+}
+add_action( 'wp_enqueue_scripts', 'greenpark_scripts_styles' );
+
+
+
+// OLD
 
 // This theme styles the visual editor with editor-style.css to match the theme style.
 add_editor_style();
